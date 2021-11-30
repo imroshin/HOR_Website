6c4297fe73984839e18f47c360a40307b44529c3
imroshin <imroshin@gmail.com>
2021-11-30 22:36:43 +0530

    added modal pop-up

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) =>{
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
+    
+    if(openBtn && modalContainer){
+        openBtn.addEventListener('click', ()=>{
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+showModal('open-modal1','modal-container')
+showModal('open-modal2','modal-container')
+showModal('open-modal3','modal-container')
+
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
